4217db01b44021086fd537c0f71a3fa73da9ff66
Vivek Birdi <vivek.birdi@novelerp.com>
2020-01-06 15:51:19 +0530

    Fixed Enquiry creation and token creationi

--- userenquery/src/main/java/com/godrej/properties/controller/OfflineEOIController.java
+++ userenquery/src/main/java/com/godrej/properties/controller/OfflineEOIController.java
@@ -2,9 +2,6 @@ package com.godrej.properties.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.godrej.properties.common.dto.CustomResponseDto;
 import com.godrej.properties.constants.KeyConstants;
 import com.godrej.properties.constants.MessageConstants;
-import com.godrej.properties.context.dto.EnquiryUserDto;
 import com.godrej.properties.dto.EnquiryDto;
 import com.godrej.properties.master.dto.ReferenceListDto;
 import com.godrej.properties.master.service.ReferenceListService;
