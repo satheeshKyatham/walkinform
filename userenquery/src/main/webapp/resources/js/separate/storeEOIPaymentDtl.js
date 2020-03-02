322572f93c38226dc7c9e5366a841f3970d9ff00
Atul33 <atulbhanushali93@gmail.com>
2020-03-02 15:47:57 +0530

    Application form changes for Meridian 2 and Add token type for meridian and belvod (pune) project

--- userenquery/src/main/webapp/resources/js/separate/storeEOIPaymentDtl.js
+++ userenquery/src/main/webapp/resources/js/separate/storeEOIPaymentDtl.js
@@ -319,10 +319,16 @@ function getTokenTypeEOI (){
 		{
 			html='<option value="F">GOLD</option><option value="F">PLATINUM</option><option value="T">EXPRESS</option>';
 		}
-	else if($("#projectsfid").val()=='a1l6F000008DnniQAC' || $("#projectsfid").val()=='a1l2s00000000pEAAQ' || $("#projectsfid").val()=='a1l6F000008iZJMQA2' || $("#projectsfid").val()=='a1l2s00000003BMAAY')
+	else if($("#projectsfid").val()=='a1l6F000004LVk8QAG' || $("#projectsfid").val()=='a1l6F000008DnniQAC' || $("#projectsfid").val()=='a1l2s00000000pEAAQ' || $("#projectsfid").val()=='a1l6F000008iZJMQA2' || $("#projectsfid").val()=='a1l2s00000003BMAAY')
 		{
 			html='<option value="F">REFUNDABLE</option><option value="T">NON-REFUNDABLE</option>';
 		}
+	else if($("#projectsfid").val()=='a1l2s00000003lPAAQ')
+	{
+		html='<option value="F">PLATINUM</option><option value="T">EXPRESS</option>';
+	}
+	
+	
 	
 	//a1l2s00000003BMAAY
 	$(".tokenTypeEOI").append(html);
