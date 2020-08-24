89213d9de8808ba7d188f54b64d80404652474a0
Rana <sumit.r@novelerp.com>
2020-08-24 17:36:17 +0530

    remove region from update payment plan

--- userenquery/src/main/webapp/resources/js/separate/updatePaymentPlan.js
+++ userenquery/src/main/webapp/resources/js/separate/updatePaymentPlan.js
@@ -8,9 +8,9 @@ $.ajaxSetup({
 var pageContext = $("#pageContext").val()+"/";	
 
 function addTnCCharge () {
-	if($('#regionList').val() != '' && $('#projectDataList').val() != '') {
+	if($('#projectDataList').val() != '') {
 		 
-		$.post(pageContext+"insertTnCForPP",{"tnc_text":$('.Editor-editor').html(),  "project_id":$('#projectDataList').val(), "project_name":$('#projectDataList :selected').text(), "pymt_plan_id":$('#ppDropdown').val(), "pymt_plan_name":$('#ppDropdown :selected').text(), "region_id":$('#regionList').val(), "region_name":$('#regionList :selected').text(),"tower_sfid":$('#towerMst').find('option:selected').attr('name')   },function(data){				 
+		$.post(pageContext+"insertTnCForPP",{"tnc_text":$('.Editor-editor').html(),  "project_id":$('#projectDataList').val(), "project_name":$('#projectDataList :selected').text(), "pymt_plan_id":$('#ppDropdown').val(), "pymt_plan_name":$('#ppDropdown :selected').text(), "region_id":$('#region_id').val(), "region_name":$('#region_id').val(),"tower_sfid":$('#towerMst').find('option:selected').attr('name')   },function(data){				 
 			
 		}).done(function(data){
 			swal({
@@ -30,7 +30,7 @@ function addTnCCharge () {
 	}
 }
 
-regionList();
+/*regionList();
 
 function regionList () {
 	$('#regionList').find("option:gt(0)").remove();
@@ -46,15 +46,15 @@ function regionList () {
 		});					
 	}).done(function() {
 	});
-}
-
+}*/
+projectDataList();
 function projectDataList (){
 	$('#projectDataList').find("option:gt(0)").remove();
 	//$('#projectDataList').empty();	
-	var urlTower = pageContext+"projectDataList?region="+$('#regionList').val();
+	var urlTower = pageContext+"projectListForSales?projectid="+ $("#gProjectId").val();
 	$.getJSON(urlTower, function (data) {
-		//$('#projectDataList').append('<option value="">Select</option>');
 		$.each(data, function (index, value) {
+			$("#region_id").val(value.region__c);
 			$('#projectDataList').append("<option value='"+value.sfid+"'>"+value.name+  " / " +value.propstrength__project_code__c+ "</option>");
 		});					
 	}).done(function() {
